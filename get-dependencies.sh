19e7fb548b8c0ec75b6b46c2ba75e2506562a3d3
Link4Electronics <link4electronics@gmail.com>
2026-05-27 01:18:52 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,9 +6,9 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm     \
-    libdbusmenu-glib        \
-    org.freedesktop.secrets
+pacman -Syu --noconfirm \
+    libdbusmenu-glib \
+    gnome-keyring
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
